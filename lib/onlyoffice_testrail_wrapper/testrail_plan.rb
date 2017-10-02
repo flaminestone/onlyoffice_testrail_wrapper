f14dc25ab6717145a56695142f86882439153a83
Pavel Lobashov <shockwavenn@gmail.com>
2017-10-02 10:36:24 +0300

    Fix typo in `TestrailPlan#plan_durations`

--- lib/onlyoffice_testrail_wrapper/testrail_plan.rb
+++ lib/onlyoffice_testrail_wrapper/testrail_plan.rb
@@ -83,7 +83,7 @@ module OnlyofficeTestrailWrapper
     # @return [Array] array of durations, sorted by descrease
     def plan_durations
       durations_hash = {}
-      [runs[0], runs[1]].each do |current_run|
+      runs.each do |current_run|
         durations_hash[current_run.name] = current_run.duration
       end
       durations_hash.sort_by { |_, time| time }.reverse
---
+++ spec/testrail/testrail_tools_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+
+describe OnlyofficeTestrailWrapper::TestrailTools do
+  it 'TestrailTools plans duration return correct number of plans' do
+    OnlyofficeTestrailWrapper::TestrailTools.configure do |testrail|
+      testrail.project = 'Test Project'
+      testrail.plan = 'Test Plan'
+    end
+    runs_durations = OnlyofficeTestrailWrapper::TestrailTools.get_runs_durations
+    runs_count = OnlyofficeTestrailWrapper::Testrail2.new
+                                                     .project('Test Project')
+                                                     .get_plan_by_name('Test Plan')
+                                                     .runs.length
+    expect(runs_durations.length).to eq(runs_count)
+  end
+end
