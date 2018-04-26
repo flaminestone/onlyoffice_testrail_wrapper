c36bab9b9d543d6b5e42855697a7d2cf62fcaba8
Pavel Lobashov <shockwavenn@gmail.com>
2018-04-26 17:31:42 +0300

    Fix RuboCop `Style/StringLiterals` cop

--- lib/onlyoffice_testrail_wrapper/testrail_helper/testrail_status_helper.rb
+++ lib/onlyoffice_testrail_wrapper/testrail_helper/testrail_status_helper.rb
@@ -7,7 +7,7 @@ module OnlyofficeTestrailWrapper
       status.each do |current_status|
         unless current_status.is_a?(Symbol)
           raise "Founded status '#{current_status}' is a '#{current_status.class}'! " \
-                "All statuses must be symbols"
+                'All statuses must be symbols'
         end
         raise 'One or some statuses is not found. Pls, check it' unless TestrailResult::RESULT_STATUSES.keys.include?(current_status)
       end
