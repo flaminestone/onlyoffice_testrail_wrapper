2710bab7f1e9530f09f428437a7b34c3bd905ea9
Pavel Lobashov <shockwavenn@gmail.com>
2018-04-26 17:30:31 +0300

    Fix RuboCop `Style/LineEndConcatenation` cop

--- lib/onlyoffice_testrail_wrapper/testrail_helper/testrail_status_helper.rb
+++ lib/onlyoffice_testrail_wrapper/testrail_helper/testrail_status_helper.rb
@@ -5,7 +5,7 @@ module OnlyofficeTestrailWrapper
     def check_status_exist(status)
       status = [status] unless status.is_a?(Array)
       status.each do |current_status|
-        raise "Founded status '#{current_status}' is a '#{current_status.class}'! " +
+        raise "Founded status '#{current_status}' is a '#{current_status.class}'! " \
               "All statuses must be symbols" unless current_status.is_a?(Symbol)
         raise 'One or some statuses is not found. Pls, check it' unless TestrailResult::RESULT_STATUSES.keys.include?(current_status)
       end
