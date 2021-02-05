58c0960bcfdd218d62289fc2524ae3753d13bb25
Pavel Lobashov <ShockwaveNN@gmail.com>
2021-02-05 16:48:27 +0300

    Fix incorrect call of project name (#128)

--- lib/onlyoffice_testrail_wrapper/testrail_project/project_cleanup.rb
+++ lib/onlyoffice_testrail_wrapper/testrail_project/project_cleanup.rb
@@ -7,9 +7,9 @@ module OnlyofficeTestrailWrapper
     # @param [Integer] days_old to close
     # @return [nil]
     def close_old_runs(days_old = 2)
-      OnlyofficeLoggerHelper.log("Going to close runs for #{@project.name}, days old: #{days_old}")
+      OnlyofficeLoggerHelper.log("Going to close runs for #{name}, days old: #{days_old}")
       runs = runs_older_than_days(days_old)
-      OnlyofficeLoggerHelper.log("Old runs number: #{runs.size} for #{@project.name}, days old: #{days_old}")
+      OnlyofficeLoggerHelper.log("Old runs number: #{runs.size} for #{name}, days old: #{days_old}")
       runs.each(&:close)
     end
   end
