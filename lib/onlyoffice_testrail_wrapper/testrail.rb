7963e94809446a06576a10156bf728134fecf648
flaminestone <kvazilife@gmail.com>
2023-06-07 11:43:58 +0200

    fix projects getting

--- lib/onlyoffice_testrail_wrapper/testrail.rb
+++ lib/onlyoffice_testrail_wrapper/testrail.rb
@@ -118,7 +118,8 @@ module OnlyofficeTestrailWrapper
     # Get all projects on testrail
     # @return [Array, ProjectTestrail] array of projects
     def get_projects
-      projects = Testrail2.http_get 'index.php?/api/v2/get_projects'
+      response = Testrail2.http_get 'index.php?/api/v2/get_projects'
+      projects = response['projects']
       @projects_names = name_id_pairs(projects) if @projects_names.empty?
       projects
     end
